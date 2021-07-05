b80183f02fe6ac1bcf964f4c81cc1364d4e57654
Luc Mosser <luc.mosser86@orange.fr>
2021-07-05 16:13:47 +0200

    react testing library lesson

--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -5,3 +5,7 @@ import '@testing-library/jest-dom/extend-expect'
 
 test("header render with correct text", () => {
 	const { getByTestId } = render(<Counter />)
+	const headerEl = getByTestId("header"); 
+
+	expect(headerEl.textContent).toBe("My Counter")
+})
